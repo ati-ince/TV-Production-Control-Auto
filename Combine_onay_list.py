da03626c0e58b3f25385d25330fdc5efd72ab422
ati <atahir.ince@gmail.com>
2019-09-18 12:54:56 +0300

    start auto detect and use excel

---
+++ Combine_onay_list.py
@@ -0,0 +1,37 @@
+import os
+
+import pandas as pd
+from pandas import ExcelWriter
+from pandas import ExcelFile
+
+import xlsxwriter
+import numpy as np
+import re
+
+#get excel in directory -----------------------------------------------------------------------
+# r=root, d=directories, f = files
+_folder = os.getcwd()
+
+def files(path):
+    for file in os.listdir(path):
+        if os.path.isfile(os.path.join(path, file)):
+            yield file
+
+for file in files("."):
+    if str(file).split('.')[-1]=='xlsx':
+        if 'cell' and 'onay' in str(file):
+            _excel_cellonay_path=_folder+"\\"+str(file)
+        elif 'panel' and 'list' in str(file):
+            _excel_panellist_path=_folder+"\\"+str(file)
+print(_excel_cellonay_path,_excel_panellist_path)
+#----------------------------------------------------------------------------------------------
+
+'''
+_filename = 'AF NX panel list 04092019 ver4'
+
+df = pd.read_excel(_filename + '.xlsx')
+
+xls = pd.ExcelFile(_filename + '.xlsx')
+
+print(xls.sheet_names)
+'''
